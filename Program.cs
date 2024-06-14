1bd4fc3ec518cee7b1afef07ac5a711506c80f5b
Setko AI <setko@msn.com>
2024-06-14 15:21:23 +0300

    Добавлены решения задач № 1 и № 2 по семинару № 6

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Pзадача № 1
+// Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+void Main()
+{
+    // Создаем двумерный массив символов
+    char[,] charArray = new char[,] { { 'a', 'b', 'c' }, { 'd', 'e', 'f' }, { 'g', 'h', 'i' } };
+
+    // Создаем строку для хранения символов массива
+    string result = "";
+
+    // Проходим через каждый элемент массива
+    for (int i = 0; i < charArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < charArray.GetLength(1); j++)
+        {
+            // Добавляем каждый символ в строку
+            result += charArray[i, j];
+        }
+    }
+
+    // Выводим результат
+    Console.WriteLine(result);
+}
+
+Main();
+
+// Задача № 2 
+// Задайте строку, содержащую латинские буквы в обоих регистрах.
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные. 
+
+
+void MainSecondTask()
+{
+    // Задаем строку с буквами в обоих регистрах
+    string originalString = "HeLLo WoRLD!";
+
+    // Преобразуем все заглавные буквы в строчные
+    string lowercaseString = originalString.ToLower();
+
+    // Выводим результат
+    Console.WriteLine(lowercaseString);
+}
+
+MainSecondTask();
+
